1d89e28e2ef39867c24dc7dc56ef8f91d54f861f
v3ctorjimmy <125600284+v3ctorjimmy@users.noreply.github.com>
2023-10-23 21:04:08 +0200

    ex01

---
+++ ft_ultimate_ft.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_ft.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: victor <victor@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/22 23:09:23 by victor            #+#    #+#             */
+/*   Updated: 2023/10/22 23:50:56 by victor           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+void	ft_ultimate_ft(int *********nbr)
+{
+	*********nbr = 42;
+}
+
+/*int	main(void)
+{
+	int nbr0;
+	int *nbr1;
+	int **nbr2;
+	int ***nbr3;
+	int ****nbr4;
+	int *****nbr5;
+	int ******nbr6;
+	int *******nbr7;
+	int ********nbr8;
+	int *********nbr9;
+	
+  nbr1 = &nbr0;
+	nbr2 = &nbr1;
+	nbr3 = &nbr2;
+	nbr4 = &nbr3;
+	nbr5 = &nbr4;
+	nbr6 = &nbr5;
+	nbr7 = &nbr6;
+	nbr8 = &nbr7;
+	nbr9 = &nbr8;
+  
+	ft_ultimate_ft(nbr9);
+	printf("%d", nbr0);
+	return(0);
+}*/
