59b58f7683c5a5fc83d32ace07916137cd83241d
v3ctorjimmy <diaz3victor@gmail.com>
2023-10-29 21:25:49 +0100

    ejercicio06 del c00

---
+++ ft_print_comb2.c
@@ -0,0 +1,48 @@
+#include <unistd.h>
+void ft_print_comb2(void)
+{
+    int a;
+    int b;
+    int c;
+    int d;
+
+    a = '0';
+    while(a <= '9')
+    {
+        b = '0';
+        while(b <= '8')
+        {
+            c = '0';
+            while(c <= '9')
+            {
+                d = b + 1;
+                while(d <= '9')
+                {
+                    if(!(a == 9 && b == 8 && c == 9 && d==9))
+                    {
+                    write(1, ", ", 1);
+                    } 
+                    write(1, &a, 1);
+                    write(1, &b, 1);
+                    write(1, " " , 1);
+                    write(1, &c, 1);
+                    write(1, &d, 1);
+                    d++;
+                }
+                
+                c++;
+            }
+          
+            b++;
+        }
+        
+        a++;
+    }
+}
+int main(void)
+{
+    ft_print_comb2();
+    return(0);
+}
+
+ 
